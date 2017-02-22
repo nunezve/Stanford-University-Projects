645084e9af681bbcb92dc4673da574f4307af3fc
nunezve <nunez.ve@icloud.com>
2017-02-21 21:30:55 -0500

    Adds basic UI elements Displays touched digits

--- Calculator_StanfordU/ViewController.swift
+++ Calculator_StanfordU/ViewController.swift
@@ -3,23 +3,49 @@
 //  Calculator_StanfordU
 //
 //  Created by Victor Nunez on 2/21/17.
-//  Copyright © 2017 io.electricdev. All rights reserved.
+//  Copyright © 2017 io.electricdev All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let mathematicalSymbol = sender.currentTitle {
+            userIsInTheMiddleOfTyping = false
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
 }
 
